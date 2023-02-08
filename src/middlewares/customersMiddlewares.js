527c6e41fc70b522be4090e722bd2b679b140c9a
Schweidzon <dannielsch@gmail.com>
2023-02-08 18:38:52 -0300

    fix: retorna erro quando passa dados validos para atualizar customer

--- src/middlewares/customersMiddlewares.js
+++ src/middlewares/customersMiddlewares.js
@@ -2,14 +2,22 @@ import { db } from "../config/database.js"
 
 export async function checkCustomerValue(req, res, next) {
     const {name, phone, cpf, birthday} = req.body
+    const {id} = req.params
     if(!name || phone.length < 10 || phone.length > 11 || cpf.length < 11 || !birthday.match(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/)) return res.sendStatus(400)
-    try {
-        const checkCustomer = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}' limit 1`)
-        console.log(checkCustomer.rows[0])
-        if(checkCustomer.rows[0]) return res.sendStatus(409)
+    if(!id) {
+        try {
+           
+            const checkCustomer = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}' limit 1`)
+            console.log(checkCustomer.rows[0])
+            if(checkCustomer.rows[0]) return res.sendStatus(409)
+            next()
+        } catch (error) {
+            res.status(500).send(error.message)
+        }
+
+    } else {
         next()
-    } catch (error) {
-        res.status(500).send(error.message)
     }
+   
 
 }

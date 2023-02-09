8bbb377ec0b4303c9140130b76224f6a054b69b1
Schweidzon <dannielsch@gmail.com>
2023-02-09 20:14:58 -0300

    fix: rota atualizar cliente

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -36,9 +36,9 @@ export async function updateCustomer(req, res) {
     const {name, phone, cpf, birthday} = req.body
     const {id} = req.params
     try {
-        const userExist = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}'`)
-        
-        if(userExist.rows[0]) return res.sendStatus(409)
+        const userExist = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id<>$2`, [cpf, id])    
+           
+        if(userExist.rowCount !== 0 ) return res.sendStatus(409)
         await db.query(`UPDATE customers SET name='${name}', phone='${phone}',cpf='${cpf}',birthday='${birthday}' WHERE id = ${id}`)
         res.send()
     } catch (error) {
